9e40e9de9efb834bb1c99ecd96239addf56cdbd8
ChiLLeR96 <p96-wenner@t-online.de>
2016-10-03 17:16:02 +0200

    [SQL]Changed integration of database relationships. - Also changed the names of some specific colums

--- install/database.sql
+++ install/database.sql
@@ -44,94 +44,111 @@ CREATE TABLE `iko_templates` (
 INSERT INTO `iko_templates` (`template_name`, `template_author`, `template_version`, `template_directory`, `template_required_core_version`)
 VALUES ('Default Template', 'IkoBB', '0.0.1', 'default', '1.0.0a');
 
-CREATE TABLE `iko_user` (
-	`user_id`                 INT(11)      NOT NULL AUTO_INCREMENT,
-	`user_name`               VARCHAR(30)  NOT NULL,
-	`user_password`           TEXT         NOT NULL,
-	`user_email`              VARCHAR(255) NOT NULL,
-	`user_avatar_id`          INT(11)               DEFAULT '1',
-	`user_signature`          VARCHAR(255)          DEFAULT NULL,
-	`user_about_user`         TEXT,
-	`user_location_id`        INT(11)               DEFAULT NULL,
-	`user_gender`             TINYINT(1)            DEFAULT NULL,
-	`user_date_joined`        INT(11)      NOT NULL,
-	`user_birthday`           DATE                  DEFAULT NULL,
-	`user_chosen_template_id` INT(11)               DEFAULT '1',
-	`user_timezone_id`        INT(11)               DEFAULT '1',
-	PRIMARY KEY (`user_id`),
-	UNIQUE KEY `user_name` (`user_name`),
-	UNIQUE KEY `user_email` (`user_email`),
-	KEY `user_avatar_id` (`user_avatar_id`),
-	KEY `user_location_id` (`user_location_id`),
-	KEY `user_chosen_template_id` (`user_chosen_template_id`),
-	KEY `user_timezone_id` (`user_timezone_id`),
-	CONSTRAINT `iko_users_ibfk_1`
-	FOREIGN KEY (`user_id`)
-	REFERENCES `iko_user_assignment` (`user_assignment_id_user`)
+
+CREATE TABLE `iko_users` (
+  `user_id`                 int(11)       NOT NULL AUTO_INCREMENT,
+  `user_name`               varchar(30)   NOT NULL,
+  `user_password`           text          NOT NULL,
+  `user_email`              varchar(255)  NOT NULL,
+  `user_avatar_id`          int(11)       DEFAULT '1',
+  `user_signature`          varchar(255)  DEFAULT NULL,
+  `user_about_user`         text,
+  `user_location_id`        int(11)       DEFAULT NULL,
+  `user_gender`             tinyint(1)    DEFAULT NULL,
+  `user_date_joined`        int(11)       NOT NULL,
+  `user_birthday`           date          DEFAULT NULL,
+  `user_chosen_template_id` int(11)       DEFAULT '1',
+  `user_timezone_id`        int(11)       DEFAULT '1',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `user_email` (`user_email`),
+  KEY `user_avatar_id` (`user_avatar_id`),
+  KEY `user_location_id` (`user_location_id`),
+  KEY `user_chosen_template_id` (`user_chosen_template_id`),
+  KEY `user_timezone_id` (`user_timezone_id`)
 );
 
+
+
 CREATE TABLE `iko_user_assignment` (
-  `user_assignment_id_user`   int(11)   NOT NULL,
-  `user_assignment_id_group`  int(11)   NOT NULL,
-  UNIQUE KEY `user_group_relation` (`user_assignment_id_user`,`user_assignment_id_group`),
-  KEY `iko_user_assignment_id_igroup` (`user_assignment_id_group`),
-  KEY `iko_user_assignment_id_user` (`user_assignment_id_user`)
-  );
+  `user_id`       int(11)   NOT NULL,
+  `usergroup_id`  int(11)   NOT NULL,
+  UNIQUE KEY `user_group_relation` (`user_id`,`usergroup_id`),
+  KEY `group_id` (`usergroup_id`),
+  KEY `user_id` (`user_id`)
+);
 
 
 CREATE TABLE `iko_usergroups` (
-  `usergroup_id`    int(11)       NOT NULL    AUTO_INCREMENT,
+  `usergroup_id`    int(11)       NOT NULL AUTO_INCREMENT,
   `usergroup_name`  varchar(255)  NOT NULL,
   `usergroup_style` varchar(255)  NOT NULL,
   PRIMARY KEY (`usergroup_id`),
-  UNIQUE KEY `iko_usergroups_usergroup_name_uindex` (`usergroup_name`),
-  CONSTRAINT `iko_usergroups_ibfk_1`
-  FOREIGN KEY (`usergroup_id`)
-  REFERENCES `iko_user_assignment` (`user_assignment_id_group`)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE
+  UNIQUE KEY `iko_usergroups_usergroup_name_uindex` (`usergroup_name`)
 );
 
 
-CREATE TABLE `iko_permissions` (
-  `permission_id`   int(11)       NOT NULL    AUTO_INCREMENT,
-  `permission_name` varchar(255)  NOT NULL,
+  CREATE TABLE `iko_permissions` (
+  `permission_id`     int(11)       NOT NULL AUTO_INCREMENT,
+  `permission_name`   varchar(255)  NOT NULL,
   PRIMARY KEY (`permission_id`),
-  UNIQUE KEY `iko_permissions_permission_name_uindex` (`permission_name`),
-  CONSTRAINT `iko_permissions_ibfk_1`
-  FOREIGN KEY (`permission_id`)
-  REFERENCES `iko_group_permissions` (`group_permission_permission_id`)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE
-  );
-
-  CREATE TABLE `iko_group_permissions` (
-  `group_permission_group_id`       int(11)   NOT NULL,
-  `group_permission_permission_id`  int(11)   NOT NULL,
-  UNIQUE KEY `group_permission_relation` (`group_permission_group_id`,`group_permission_permission_id`),
-  KEY `iko_group_permission_id_permission` (`group_permission_permission_id`),
-  KEY `iko_group_permission_id_group` (`group_permission_group_id`),
-  CONSTRAINT `iko_group_permissions_ibfk_1`
-  FOREIGN KEY (`group_permission_group_id`)
-  REFERENCES `iko_usergroups` (`usergroup_id`)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE
+  UNIQUE KEY `iko_permissions_permission_name_uindex` (`permission_name`)
 );
+
+CREATE TABLE `iko_group_permissions` (
+  `usergroup_id`    int(11)   NOT NULL,
+  `permission_id`   int(11)   NOT NULL,
+  UNIQUE KEY `group_permission_relation` (`usergroup_id`,`permission_id`),
+  KEY `group_permission_id` (`permission_id`),
+  KEY `iko_group_permission_id_group` (`usergroup_id`)
+);
+
 
 CREATE TABLE `iko_user_permissions` (
-  `user_permission_user_id`         int(11)   NOT NULL,
-  `user_permission_permission_id`   int(11)   NOT NULL,
-  UNIQUE KEY `group_permission_relation` (`user_permission_permission_id`,`user_permission_user_id`),
-  KEY `iko_group_permission_id_permission` (`user_permission_permission_id`),
-  KEY `iko_group_permission_id_group` (`user_permission_user_id`),
-  CONSTRAINT `iko_user_permissions_ibfk_1`
-  FOREIGN KEY (`user_permission_user_id`)
+  `user_id`         int(11)     NOT NULL,
+  `permission_id`   int(11)     NOT NULL,
+  UNIQUE KEY `group_permission_relation` (`permission_id`,`user_id`),
+  KEY `iko_group_permission_id_permission` (`permission_id`),
+  KEY `iko_group_permission_id_group` (`user_id`)
+);
+
+/*Relation between Tables */
+
+ALTER Table iko_user
+ADD CONSTRAINT `iko_users_ibfk_1`
+	FOREIGN KEY (`user_id`)
+	REFERENCES `iko_user_assignment` (`user_id`);
+
+
+ALTER TABLE iko_group_permissions
+ADD CONSTRAINT `iko_group_permissions_ibfk_1`
+  FOREIGN KEY (`usergroup_id`)
+  REFERENCES `iko_usergroups` (`usergroup_id`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE iko_usergroups
+ADD CONSTRAINT `iko_usergroups_ibfk_1`
+  FOREIGN KEY (`usergroup_id`)
+  REFERENCES `iko_user_assignment` (`usergroup_id`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+
+ALTER TABLE iko_permissions
+ADD CONSTRAINT `iko_permissions_ibfk_1`
+  FOREIGN KEY (`permission_id`)
+  REFERENCES `iko_group_permissions` (`permission_id`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+  ALTER TABLE iko_user_permissions
+  ADD CONSTRAINT `iko_user_permissions_ibfk_1`
+  FOREIGN KEY (`user_id`)
   REFERENCES `iko_users` (`user_id`)
   ON UPDATE CASCADE,
-  CONSTRAINT `iko_user_permissions_ibfk_2`
-  FOREIGN KEY (`user_permission_permission_id`)
+  ADD `iko_user_permissions_ibfk_2`
+  FOREIGN KEY (`permission_id`)
   REFERENCES `iko_permissions` (`permission_id`)
   ON DELETE CASCADE
-  ON UPDATE CASCADE
-  );
-
+  ON UPDATE CASCADE;
