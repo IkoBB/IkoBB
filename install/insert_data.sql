c26e63ab6c2a0dc483c4b3ed44887540ad40fcd2
Jannik <jannik.renfordt@ikobb.de>
2016-10-29 14:45:57 +0200

    [SQL] Deleted wrong commas

--- install/insert_data.sql
+++ install/insert_data.sql
@@ -7,23 +7,18 @@ INSERT INTO `iko_modules` VALUES ('IkoBB','iko','Core Engine','1.0.0a',1),
 INSERT INTO `iko_configs` VALUES ('site_name', 's:10:"Test Value";', 'The name of the site', 'iko'),
 	('site_template', "i:1;", 'Insert the ID of the template which should be the default template.', 'iko'),
 	('site_email', 's:13:"test@test.com";', 'The contact email of the forum. Also used for sending emails.', 'iko'),
-	('site_maintenance', "i:0;", 'Indicates if the site is maintenance. 1 - Maintenance Mode on; 2 - Maintenance Mode off','iko'),
-	;
+	('site_maintenance', "i:0;", 'Indicates if the site is maintenance. 1 - Maintenance Mode on; 2 - Maintenance Mode off','iko');
 
 INSERT INTO `iko_templates` (`template_name`, `template_author`, `template_version`, `template_directory`, `template_required_core_version`)
-VALUES ('Default Template', 'IkoBB', '0.0.1', 'default', '1.0.0a'),
-;
+VALUES ('Default Template', 'IkoBB', '0.0.1', 'default', '1.0.0a');
 
-INSERT INTO `iko_users` VALUES ('1','Administrator','admin_password','admin@ikobb.de','1','','','1','1','11021996','12.09.1998','1'),
-;
+INSERT INTO `iko_users` VALUES ('1','Administrator','admin_password','admin@ikobb.de','1','','','1','1','11021996','12.09.1998','1');
 
 INSERT INTO `iko_usergroups` VALUES ('1','Admin','<span class="color: red; ">%% group_name %%</span>'),
 ('2','Moderator','<span class="color: green; ">%% group_name %%</span>'),
-('3','User','%% group_name %%'),
-;
+('3','User','%% group_name %%');
 
-INSERT INTO `iko_user_assignment` VALUES ('1','1'),
-;
+INSERT INTO `iko_user_assignment` VALUES ('1','1');
 
 /*
 INSERT INTO `iko_group_assignment` VALUES ('1','2'),
