37934a02f8c888c32aa014988c72928d985d8d85
ChiLLeR96 <p96-wenner@t-online.de>
2016-11-03 21:32:04 +0100

    [SQL]Fixed error because of the new column last_login at the insert_data.sql from the user table [PHP]Tested language engine

--- install/insert_data.sql
+++ install/insert_data.sql
@@ -13,7 +13,7 @@ INSERT INTO `iko_configs` VALUES ('site_name', 's:10:"Test Value";', 'The name o
 INSERT INTO `iko_templates` (`template_name`, `template_author`, `template_version`, `template_directory`, `template_required_core_version`)
 VALUES ('Default Template', 'IkoBB', '0.0.1', 'default', '1.0.0a');
 
-INSERT INTO `iko_users` VALUES ('1','Administrator','admin_password','admin@ikobb.de','1','','','1','1','11021996','12.09.1998','1');
+INSERT INTO `iko_users` VALUES ('1','Administrator','admin_password','admin@ikobb.de','1','','','1','1','11021996','12.09.1998','1','');
 
 INSERT INTO `iko_usergroups` VALUES ('1','Admin','<span class="color: red; ">%% group_name %%</span>'),
 ('2','Moderator','<span class="color: green; ">%% group_name %%</span>'),
--- test.php
+++ test.php
@@ -4,9 +4,9 @@ namespace Iko;
 use Iko\Permissions\User as PUser;
 
 require_once 'core/core.php';
-module::request("user");
-$puser = User::get("MatPlayTV");
-var_dump($puser);
+module::request("translation");
+//$puser = User::get("MatPlayTV");
+//var_dump($puser);
 /*$permuser = PUser::get($puser);
 print_r($permuser);
 var_dump($permuser->has_Permission("iko.*"));
