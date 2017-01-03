2d352264cd6c58fdfb23560df2527394c91e83b9
Matzestyler <marcel.nowocyn@googlemail.com>
2017-01-03 21:04:25 +0100

    [Handler] Added new Function event_module($name, $module, $args, $init_args); That Function will only trigger events from named $module and returns the latest value like before.

--- core/event/handler.class.php
+++ core/event/handler.class.php
@@ -44,10 +44,19 @@ class Handler
 
 	public static function event ($name, $args = NULL, $init_Args = NULL)
 	{
-		return self::trigger(__FUNCTION__, $name, $args, $init_Args);
+		return self::trigger(__FUNCTION__, $name, NULL, $args, $init_Args);
 	}
 
-	private static function trigger ($type, $name, $args = NULL, $init_Args = NULL)
+	public static function event_module ($name, $module, $args = NULL, $init_Args = NULL)
+	{
+		if ($module instanceof module) {
+			$module = $module->get_name();
+		}
+
+		return self::trigger(__FUNCTION__, $name, $module, $args, $init_Args);
+	}
+
+	private static function trigger ($type, $name, $module_name = NULL, $args = NULL, $init_Args = NULL)
 	{
 		//Set the needed static array as $array
 		switch ($type) {
@@ -56,6 +65,9 @@ class Handler
 			break;
 			case 'FinalAll':
 				$array = &self::$FinalAll;
+			break;
+			case 'event_module':
+				$array = &self::$event;
 			break;
 			default:
 				$array = &self::$event;
@@ -71,44 +83,45 @@ class Handler
 			$false_counter = 0;
 			foreach ($array[ $name ] as $value) {
 				//load Complete Module at start and all needed Data from the array
-				$module = module::get($value["module"]);
-				if (!$module->is_load()) {
-					$module->load_complete();
-				}
-				$class = $value["class"];
-				$function = $value["function"];
-				//This statement will be used for registered Events with no static function to use
-				if (!isset($value["is_func_static"]) || $value["is_func_static"] == FALSE) {
-					//Create a ReflectionClass from the original one to get new Instance and to handle it better
-					$reflection = new \ReflectionClass($class);
-					//If there isset an instance and it's equal with the same class it will set it
-					if (isset($value["instance"]) && $value["instance"] != NULL && get_class($value["instance"]) == $reflection->getName()) {
-						$class = $value["instance"];
+				if ($type != 'event_module' || ($type == 'event_module' && $module_name == $value["module"])) {
+					$module = module::get($value["module"]);
+					if (!$module->is_load()) {
+						$module->load_complete();
 					}
-					//If the Class can get over a special static function it will use them from the class. It will get as a return value from the function eval
-					elseif (isset($value["can_init_over"]) && $value["can_init_over"] != NULL) {
-						$class = eval("return " . $class . "::" . $value["can_init_over"] . "(" . $init_Args . ");");
+					$class = $value["class"];
+					$function = $value["function"];
+					//This statement will be used for registered Events with no static function to use
+					if (!isset($value["is_func_static"]) || $value["is_func_static"] == FALSE) {
+						//Create a ReflectionClass from the original one to get new Instance and to handle it better
+						$reflection = new \ReflectionClass($class);
+						//If there isset an instance and it's equal with the same class it will set it
+						if (isset($value["instance"]) && $value["instance"] != NULL && get_class($value["instance"]) == $reflection->getName()) {
+							$class = $value["instance"];
+						}
+						//If the Class can get over a special static function it will use them from the class. It will get as a return value from the function eval
+						elseif (isset($value["can_init_over"]) && $value["can_init_over"] != NULL) {
+							$class = eval("return " . $class . "::" . $value["can_init_over"] . "(" . $init_Args . ");");
+						}
+						//The simplest way is to create a new instance like $var = new class($init_args); with the reflectionclass
+						else {
+							$class = $reflection->newInstance($init_Args);
+						}
+						//Will call the function that will be used with the Event_name the original args and the edited or the previous bool statement
+						$var = $class->$function($name, $args, $var);
 					}
-					//The simplest way is to create a new instance like $var = new class($init_args); with the reflectionclass
+					//Will call the function over eval with the same parameter like in a normal class.
 					else {
-						$class = $reflection->newInstance($init_Args);
+						$var = eval("return " . $class . "::" . $function . "(" . var_export($name,
+								TRUE) . "," . var_export($args, TRUE) . ", " . var_export($var, TRUE) . ");");
 					}
-					//Will call the function that will be used with the Event_name the original args and the edited or the previous bool statement
-					$var = $class->$function($name, $args, $var);
-				}
-				//Will call the function over eval with the same parameter like in a normal class.
-				else {
-					$var = eval("return " . $class . "::" . $function . "(" . var_export($name,
-							TRUE) . "," . var_export($args,
-							TRUE) . ", " . var_export($var, TRUE) . ");");
-				}
-				//If the returned $var is a bool statement it will be handled with the $false_counter
-				if ($type == "event" && $var === FALSE) {
-					$false_counter++;
+					//If the returned $var is a bool statement it will be handled with the $false_counter
+					if ($type == "event" && $var === FALSE) {
+						$false_counter++;
+					}
 				}
 			}
 			//Will set the $result as true if $false_counter is lower than 1 and $false_counter is not the same like the sum of the $array[$name] / Event
-			if ($type == "event" && $false_counter != count($array[ $name ]) && $false_counter < 1) {
+			if (($type == "event" || $type == "event_module") && $false_counter != count($array[ $name ]) && $false_counter < 1) {
 				$result = TRUE;
 			}
 		}
@@ -116,7 +129,7 @@ class Handler
 			return TRUE;
 		}
 		//After all Done a normal Event get's a real return value but only if something was set
-		if ($type == "event") {
+		if ($type == "event" || $type == "event_module") {
 			if (count($array[ $name ]) > 0) {
 				if (isset($var) && !is_bool($var)) {
 					return $var;
@@ -134,7 +147,7 @@ class Handler
 
 	public static function event_Final ($name, $args = NULL, $init_Args = NULL)
 	{
-		self::trigger(__FUNCTION__, $name, $args, $init_Args);
+		self::trigger(__FUNCTION__, $name, NULL, $args, $init_Args);
 	}
 
 	public static function FinalAll ()
